4ac8d979af9fe599b346abd9caa409ea5cf8ddb2
Lillian Qiao <lillianq@usc.edu>
2024-11-04 16:12:20 -0800

    congrats page functionality

---
+++ congrats.js
@@ -0,0 +1,11 @@
+//event listener for "Play Another Game" button
+document.getElementById('play-again').addEventListener('click', function() {
+    window.location.href = 'newgame.html'; // directs back to DT Games homepage?
+  });
+  
+  //event listener for "Go Back to Crossword" button
+  document.getElementById('back-to-cw').addEventListener('click', function() {
+    window.location.href = 'minicrossword.html'; 
+  });
+
+  
